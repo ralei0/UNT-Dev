4c9cf881bd8816df624072a6c323e9783d1f9580
Likhithaxf <123699634+Likhithaxf@users.noreply.github.com>
2023-03-20 10:40:01 -0500

    Multer library to upload and manage img

---
+++ Source Code/middlewares/multer.js
@@ -0,0 +1,26 @@
+const multer = require('multer')
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './images');
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().toISOString() + '-' + file.originalname);
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+var multerupload = multer({ storage: fileStorage, fileFilter: fileFilter }).single('imageUp')
+module.exports = multerupload
+
+//app.use(multer({ storage: fileStorage, fileFilter: fileFilter }).single('imageUp'));
