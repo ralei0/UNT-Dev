1112733e5e27bf7b81c9c1490eb538b2c66dee74
k1r4n20ma <123656717+k1r4n20ma@users.noreply.github.com>
2023-03-20 01:38:03 -0500

    updated userAuth.js

---
+++ Source Code/controllers/user/userAuth.js
@@ -0,0 +1,178 @@
+const bcrypt = require('bcryptjs');
+const User = require('../../models/User');
+const nodemailer = require('nodemailer');
+
+var transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'rajatmohantyrm768@gmail.com',
+        pass: 'huibjwgevwwcaozp'
+    }
+});
+
+
+exports.getLogin = (req, res, next) => {
+
+    res.render('user/login', {
+        pageTitle: 'Login',
+    });
+
+};
+
+exports.getRegister = (req, res, next) => {
+    res.render('user/register', {
+        pageTitle: 'Register',
+    });
+
+};
+
+
+
+exports.postRegister = (req, res, next) => {
+
+
+    const name = req.body.name;
+    const email = req.body.email;
+    const password = req.body.password;
+    const password2 = req.body.password2;
+    const time = new Date().toISOString().split('T')[0];
+
+    let errors = [];
+
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: "Please fill all the fields." });
+    }
+
+    if (password !== password2) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
+
+    if (password.length < 5) {
+        errors.push({ msg: 'Passwords should be atleast 6 characters' });
+    }
+
+    if (errors.length > 0) {
+        res.render('user/register', {
+            pageTitle: 'Register',
+            errors,
+            name,
+            email,
+            password,
+            password2
+
+        });
+    } else {
+        User.findOne({ email: email })
+            .then(user => {
+                if (user) {
+                    errors.push({ msg: 'User is already resisterd with this email.' });
+                    res.render('user/register', {
+                        pageTitle: 'Register',
+                        errors,
+                        name,
+                        email,
+                        password,
+                        password2
+
+                    });
+                } else {
+                    const cart = {};
+                    const newUser = new User({ //instance of the user model
+                        name,
+                        email,
+                        password,
+                        cart,
+                        time
+                    });
+
+                    //hasing password and saving user in database
+                    bcrypt.genSalt(10, (err, salt) => {
+                        bcrypt.hash(newUser.password, salt, (err, hash) => {
+                            if (err)
+                                throw err;
+
+                            newUser.password = hash;
+
+                            newUser.save()
+                                .then(user => {
+                                    var mailOptions = {
+                                        from: 'rajatmohantyrm768@gmail.com',
+                                        to: email,
+                                        subject: 'Thank you for Registration',
+                                        html: 'Your Registration is success'
+                                    };
+
+                                    transporter.sendMail(mailOptions, function(error, info) {
+                                        if (error) {
+                                            console.log(error);
+                                        } else {
+                                            console.log('Email sent: ' + info.response);
+                                        }
+                                    });
+                                    req.flash('success_msg', 'You are registered Successfully!');
+                                    res.redirect('/login');
+                                })
+                                .catch((err) => { //console.log(err)
+                                    req.flash('error_msg', 'Something wrong happend.');
+                                    res.redirect('/home');
+                                });
+                        })
+                    });
+                }
+            })
+            .catch((err) => { //console.log(err)
+                req.flash('error_msg', 'Something wrong happend.');
+                res.redirect('/home');
+            });
+    }
+
+
+
+
+};
+
+
+
+exports.postLogin = (req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    User.findOne({ email: email })
+        .then(user => {
+            if (!user) {
+                req.flash('error_msg', 'This is not registerd email Id.');
+                return res.redirect('/login');
+            }
+            bcrypt
+                .compare(password, user.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        req.session.isUserLoggedIn = true;
+                        req.session.user = user;
+
+                        return req.session.save(err => {
+                            res.redirect('/home');
+                        });
+                    }
+                    req.flash('error_msg', 'Wrong Password.');
+                    res.redirect('/login');
+                })
+                .catch(err => {
+                    //console.log(err);
+                    req.flash('error_msg', 'Something wrong happened , please retry again.');
+                    res.redirect('/login');
+                });
+        })
+        .catch((err) => { //console.log(err)
+            req.flash('error_msg', 'Something wrong happend.');
+            res.redirect('/home');
+        });
+
+};
+
+
+exports.postLogout = (req, res, next) => {
+    req.session.destroy(err => {
+        //console.log(err);
+        res.redirect('/');
+    });
+};
