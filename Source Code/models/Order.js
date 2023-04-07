e4e803f246a21baec65fce5fb541fc26134c69ba
ralei0 <56300491+ralei0@users.noreply.github.com>
2023-04-07 16:38:22 -0500

    Backend for order

---
+++ Source Code/models/Order.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema({
+  products: [
+    {
+      product: { type: Object, required: true },
+      quantity: { type: Number, required: true }
+    }
+  ],
+  user: {
+    email: {
+      type: String,
+      required: true
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      required: true,
+      ref: 'User'
+    }
+  },
+  total:{
+    type:Number
+  },
+  date: {
+        type: Date,
+        default: Date.now
+      }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
