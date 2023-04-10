0eacaee8d3a1294c072c78505d633c83350167bb
ralei0 <56300491+ralei0@users.noreply.github.com>
2023-04-10 10:10:09 -0500

    Update index.js

--- Source Code/routes/index.js
+++ Source Code/routes/index.js
@@ -1,11 +1,12 @@
 //for user routes
-const express = require('express');
+const express= require('express');
 const shopController = require('../controllers/index');
-const isCheck = require('../middlewares/index/isCheck');
-const router = express.Router();
+const shopController2 = require('../controllers/user/shop');
+const isCheck= require('../middlewares/index/isCheck');
+const router= express.Router();
 
-router.get('/', isCheck, shopController.getIndex);
-//router.get('/products/:productId', shopController2.getProduct); //dynamic routtes uses controller
+router.get('/',isCheck,shopController.getIndex);
+router.get('/products/:productId',shopController2.getProduct); //dynamic routtes uses controller
 
 
 module.exports = router;
