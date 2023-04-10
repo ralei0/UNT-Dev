b69a0363da9302b248adbc6e4cb90b32adff9419
ralei0 <56300491+ralei0@users.noreply.github.com>
2023-04-10 10:08:36 -0500

    Create shop.js

---
+++ Source Code/routes/user/shop.js
@@ -0,0 +1,18 @@
+const express= require('express');
+const shopController = require('../../controllers/user/shop');
+const isAuth = require('../../middlewares/user/isAuth');
+
+const router= express.Router();
+
+router.get('/home',isAuth,shopController.getHome);
+router.get('/cart',isAuth,shopController.getCart);
+router.post('/cart',isAuth,shopController.postCart);
+router.post('/delete-cart-item',isAuth,shopController.postCartDeleteItem);
+router.get('/orders',isAuth,shopController.getOrders);
+ // router.get('/checkout',shopController.getCheckout);
+router.post('/create-order',isAuth,shopController.postOrder);
+router.get('/products/:productId',shopController.getProduct); //dynamic routtes uses controller
+
+
+
+module.exports = router;
