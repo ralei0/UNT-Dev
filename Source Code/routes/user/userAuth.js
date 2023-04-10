4f11f2694f44811fe58f5b9398603104f6bfb9e6
ralei0 <56300491+ralei0@users.noreply.github.com>
2023-04-10 10:09:12 -0500

    Update userAuth.js

--- Source Code/routes/user/userAuth.js
+++ Source Code/routes/user/userAuth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../../controllers/user/userAuth');
-//const profileController = require('../../controllers/user/profile');
+const profileController = require('../../controllers/user/profile');
 const isAuth = require('../../middlewares/user/isAuth');
 const isCheck = require('../../middlewares/user/isCheck');
 
@@ -10,8 +10,8 @@ router.post('/login', isCheck, authController.postLogin);
 router.get('/register', isCheck, authController.getRegister);
 router.post('/register', isCheck, authController.postRegister);
 router.post('/logout', isAuth, authController.postLogout);
-//router.post('/profile', profileController.createProfile2);
-//router.get('/profile', profileController.createProfile);
+router.post('/profile', profileController.createProfile2);
+router.get('/profile', profileController.createProfile);
 
 
 
