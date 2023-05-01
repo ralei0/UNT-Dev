afa8cdac4b7c4843f16065686db67a257d1a4708
girishggithub <123661283+girishggithub@users.noreply.github.com>
2023-05-01 11:15:15 -0500

    Update keys.js

--- Source Code/config/keys.js
+++ Source Code/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
     MongoURI: 'mongodb://localhost:27017/Online_shopping'
+    // MongoURI: 'mongodb+srv://chavanvinayak017:chavanvinayak017@cluster0.venjqbu.mongodb.net/test'
 }
