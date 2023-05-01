bb99c5b6969315295d248d50f922b70a4e94b711
girishggithub <123661283+girishggithub@users.noreply.github.com>
2023-05-01 11:17:56 -0500

    Update index.js

--- Source Code/controllers/index.js
+++ Source Code/controllers/index.js
@@ -1,19 +1,19 @@
 const Product = require('../models/Product');
+const Order = require('../models/Order');
 
-
-exports.getIndex = (req, res, next) => {
-
+exports.getIndex = (req,res,next) => {
+   
     Product.find()
-        .then(products => {
-
-            res.render('index', {
-                product: products,
-                pageTitle: 'Index',
-            });
-        })
-        .catch((err) => { //console.log(err)
-            req.flash('error_msg', 'Something wrong happend.');
-            req.redirect('/home');
+    .then(products => {
+            
+            res.render('index',{
+            product:products,
+            pageTitle:'Index',
+        });
+    })
+    .catch((err) =>{//console.log(err)
+        req.flash('error_msg','Something wrong happend.');
+        req.redirect('/home');
         });
 
 }
